cb74ff9b72780aa4e544e96cb71f80896aebf5f2
vuduclyunitn <ducly.vu@unitn.it>
2017-12-04 16:40:52 -0500

    Normalize data using min max scaler

--- nomarlizeinput.py
+++ nomarlizeinput.py
@@ -1,7 +1,7 @@
 
 
-dataset = [[1, 2, 3, 4], [1,100, 3, 3], [10, 12, 0, 4]]
-
+#dataset = [[1, 2, 3, 4], [1,100, 3, 3], [10, 12, 0, 4]]
+dataset = [[50, 30], [20, 90]]
 def getMinMax(dataset):
     minmax = []
     for col in range(len(dataset[0])):
@@ -11,5 +11,13 @@ def getMinMax(dataset):
         minmax.append((min(temp), max(temp)))
     return minmax
 
-mm = getMinMax(dataset)
-print(mm)
+def rescale(dataset):
+    for col in range(len(dataset[0])):
+        (mi, ma ) = getMinMax(dataset)[col]
+        print(mi, ma)
+        for row in range(len(dataset)):
+            scaledValue = (dataset[row][col] - mi) / (ma - mi)
+            print("Scaled value: {}".format(scaledValue))
+
+mm = rescale(dataset)
+#print(mm)
